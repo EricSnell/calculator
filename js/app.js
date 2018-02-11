a2047c7f3685947f895f6799857061d850966402
eric <ericmichaelsnell@gmail.com>
2018-02-11 16:34:52 -0500

    writing out logic

---
+++ js/app.js
@@ -0,0 +1,23 @@
+(() => {
+  /* AC --- Clears input and total
+     CE --- Clears current input (not total)
+
+     - Variables
+          = current input
+          = total
+          = decimal flag
+
+     - Event listener on each button (click)
+     - Check which ID was clicked
+     - If operator - next button cannot be another operator
+     - If decimal - current cannot already contain decimal (check flag)
+     - If number - append number to current input var 
+     - If operator, take current input and set as total plus operator
+     - If AC - reset all values
+     - If CE - clear input only (clear all if pressed again)
+     - If equal - run the total and set the current input to result
+     - First number of total cannot be zero
+  */
+
+
+})();
