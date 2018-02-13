db6474bfba46f446c2fc6675da1a935944d0342d
eric <ericmichaelsnell@gmail.com>
2018-02-13 16:49:11 -0500

    minor additions to functionality

--- js/app.js
+++ js/app.js
@@ -1,47 +1,45 @@
 (() => {
-  /* AC --- Clears input and total
-     CE --- Clears current input (not total)
-
-     - Variables
-          = current input
-          = total
-          = decimal flag
-
+  /* 
      - If operator - next button cannot be another operator
      - If operator, take current input and set as total plus operator
-     - If AC - reset all values
-     - If CE - clear input only (clear all if pressed again)
-     - If equal - run the total and set the current input to result
      - First number of total cannot be zero
   */
 
-  const state = {
-    current: '',
-    decimal: false,
-    total: ''
-  }
 
   const [buttons] = Array.from(document.getElementsByClassName('buttons'));
   const [inputDisplay] = document.getElementsByClassName('display__current');
   const [totalDisplay] = document.getElementsByClassName('display__total');
+
+  let state = {
+    current: '',
+    decimal: false,
+    operator: false,
+    total: ''
+  }
+
+
+
 
   buttons.addEventListener('click', (e) => {
     let input;
     const isNumber = e.target.classList.contains('btn--number') && e.target.id !== '.';
     const isDecimal = e.target.id === '.';
     const isOperator = e.target.classList.contains('btn--operator');
+    const isCE = e.target.id === 'CE';
+    const isAC = e.target.id === 'AC';
+
+    // if current state is an operator, then reset to avoid appending to 
+    if (!isNotOperator(state.current)) {
+      resetCurrent();
+    }
 
     if (isNumber) {
       input = e.target.id;
       // if current state is a number or if it's empty
-      if (typeof parseInt(state.current) === 'number' || state.current.length === 0) {
-        if (isNotOperator(state.current)) {
-          state.current += input;
-        } else {
-          state.current = e.target.id;
-        }
+      if (typeof parseFloat(state.current) === 'number' || state.current.length === 0) {
+        state.current += input;
+        state.total += input;
       }
-      state.total += input;
     }
 
     else if (isDecimal) {
@@ -56,12 +54,16 @@
     }
 
     else if (isOperator) {
-      // check if current state is already an operator
-      if (isNotOperator(state.current) && e.target.id === '=') {
+      // check if current state is a number and that the equal sign was pressed
+      if (typeof parseFloat(state.current) === 'number' && e.target.id === '=') {
+        console.log('equal sign pressed');
         const answer = eval(state.total);
+        state.decimal = false;
         state.total += `=${answer}`;
         state.current = answer;
       } else if (isNotOperator(state.current)) {
+        console.log('current is not already operator');
+        state.decimal = false;
         state.current = e.target.id;
         state.total += e.target.id;
       } else {
@@ -73,15 +75,48 @@
       // set answer to the display
     }
 
+    else if (isAC) {
+      clearAll();
+    }
+
+    else if (isCE) {
+      if (state.current.length) {
+        resetCurrent();
+      } else {
+        clearAll();
+      }
+    }
+
     console.log('state:', state);
     inputDisplay.innerText = state.current;
     totalDisplay.innerText = state.total;
 
   });
 
+
+
+
+
+
+
   function isNotOperator(input) {
     const operators = ['+', '-', '*', '/', '='];
+    console.log('input>>>', input);
     return !operators.includes(input);
+  }
+
+  function resetCurrent() {
+    state = Object.assign({}, state, { current: '' });
+  }
+
+  function clearAll() {
+    const newState = {
+      current: '',
+      decimal: false,
+      operator: false,
+      total: ''
+    }
+    state = { ...newState };
   }
 
 
