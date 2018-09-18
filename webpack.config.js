e81f42cd6d3d1ef0acb6c6bed5b678907c5af462
eric <ericmichaelsnell@gmail.com>
2018-09-17 20:46:10 -0400

    webpack config

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+const pug = {
+  test: /\.pug$/,
+  use: ['html-loader?attrs=false', 'pug-html-loader']
+};
+
+const scss = {
+  test: /\.scss$/,
+  use: ['style-loader', 'css-loader', 'sass-loader']
+};
+
+module.exports = {
+  entry: './src/js/app.js',
+
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  module: {
+    rules: [pug, scss]
+  },
+
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: 'src/pug/index.pug'
+    }),
+    new MiniCssExtractPlugin({
+      filename: 'style.css'
+    }),
+    new CleanWebpackPlugin(['dist'])
+  ]
+};
